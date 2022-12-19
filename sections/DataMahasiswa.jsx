a8dba86c428e25efc2fc01de9799faa3a60ab587
alvinirwanto <irwantoalvin@gmail.com>
2022-12-19 14:34:57 +0700

    add footer and link in button of hero section

--- sections/DataMahasiswa.jsx
+++ sections/DataMahasiswa.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const DataMahasiswa = () => {
     return (
-        <div className='mt-[15rem]'>
+        <div name='data-mahasiswa' className='mt-[5rem]'>
             <iframe 
             title="All student dashboard 161222 - Halaman 1" 
             // width="600" height="373.5" 
-            className='w-full h-screen scale-[1.1]'
+            className='w-full h-[200vh]'
             src="https://app.powerbi.com/view?r=eyJrIjoiY2I5ODVmNDItY2ZjOS00N2RkLWIxMWItYjExNGQwZjBiMWZkIiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
 
         </div>
--- sections/Heropage.jsx
+++ sections/Heropage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
 
+import { Link } from 'react-scroll'
+
 import { IoArrowForward } from 'react-icons/io5'
 
 const Heropage = () => {
@@ -10,10 +12,17 @@ const Heropage = () => {
                 <h1 className='text-xl font-semibold text-primary-green'>Selamat datang di</h1>
                 <p className='text-3xl xl:text-5xl font-bold text-primary-blue xl:leading-[3.5rem]'>Pangkalan Data <br /> Universitas Mercu Buana</p>
                 <p className='mt-5'>Pangkalan data Universitas Mercu Buana merupakan sajian informasi yang menyajikan data dan statistik tentang Universitas Mercu Buana secara aktual, terbaru, dan terpercaya.</p>
-                <button className='self-start flex gap-4 items-center mt-8 bg-primary-blue text-white rounded-md px-6 py-4'>
-                    Lihat lebih lanjut
-                    <IoArrowForward />
-                </button>
+                <Link 
+                to='data-mahasiswa'
+                offset={-170}
+                smooth={true}
+                duration={500}
+                >
+                    <button className='self-start flex gap-4 items-center mt-8 bg-primary-blue text-white rounded-md px-6 py-4'>
+                        Lihat lebih lanjut
+                        <IoArrowForward />
+                    </button>
+                </Link>
             </div>
             <div className=''>
                 <Image
