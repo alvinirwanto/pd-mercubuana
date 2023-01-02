6b06193e3e53e82652b4e7c2e0ee1f963cb5e96d
Febe Putrianne <putrifebe@gmail.com>
2023-01-02 10:27:41 +0700

    edit pp

--- sections/KeuanganPermintaanPembayaran.jsx
+++ sections/KeuanganPermintaanPembayaran.jsx
@@ -7,7 +7,7 @@ const KeuanganPermintaanPembayaran = () => {
                 title="Dashboard Keuangan Mahasiswa"
                 className='w-full h-screen scale-[1.1]'
                 // width="600" height="373.5"
-                src="https://app.powerbi.com/view?r=eyJrIjoiMjdlNmFiNDEtNGUyMy00OTE3LWE0YzAtODFhODc0OWIyZDQ4IiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>
+                src="https://app.powerbi.com/view?r=eyJrIjoiOWYxNmU5NDMtZmQ2MC00Y2ZlLWEwYTAtY2U3NWRjZWE4Yjc3IiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
 
         </div>
     )
