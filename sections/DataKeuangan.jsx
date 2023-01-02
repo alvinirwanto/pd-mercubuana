d22f86e06bf93b5db13051209fe69a9d97b7de69
Febe Putrianne <putrifebe@gmail.com>
2023-01-02 10:42:34 +0700

    test

--- sections/DataKeuangan.jsx
+++ sections/DataKeuangan.jsx
@@ -4,7 +4,7 @@ const DataKeuangan = () => {
     return (
         <div className='mt-[10rem]'>
             <iframe
-                title="Dashboard Keuangan Mahasiswa"
+                title="Dashboard Keuangan Mahasiswa test"
                 className='w-full h-screen scale-[1.1]'
                 // width="600" height="373.5"
                 src="https://app.powerbi.com/view?r=eyJrIjoiMjdlNmFiNDEtNGUyMy00OTE3LWE0YzAtODFhODc0OWIyZDQ4IiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>
