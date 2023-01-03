cb7278af5bd20d8cbd68054b387810d9138bdc0d
alvinirwanto <irwantoalvin@gmail.com>
2023-01-03 15:32:05 +0700

    edit the page and sections

--- pages/mahasiswa/keuangan.jsx
+++ pages/mahasiswa/keuangan.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Layout from '../../components/Layout'
-import DataKeuangan from '../../sections/DataKeuangan'
 
 const Keuangan = () => {
     return (
         <Layout
             pageTitle='Data Keuangan'>
-            <DataKeuangan/>
+            <div className='mt-[10rem]'>
+                <iframe
+                    title="Dashboard Keuangan Mahasiswa"
+                    className='w-full h-screen scale-[1.1]'
+                    // width="600" height="373.5"
+                    src="https://app.powerbi.com/view?r=eyJrIjoiNDBmZTZmZTMtNWMxYi00YjQ5LWJiN2QtOTYwMGU2MDRkZjgzIiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+
+            </div>
         </Layout>
     )
 }
