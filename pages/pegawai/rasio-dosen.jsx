a5e0bad7f9ce44488843a4df2bdd40b9e5c19a41
Febe Putrianne <putrifebe@gmail.com>
2023-01-03 18:08:18 +0700

    gittest

---
+++ pages/pegawai/rasio-dosen.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const RasioDosen = () => {
+    return (
+        <Layout
+            pageTitle='Rasio Dosen'
+        >
+            <div className='pt-[10rem]'>
+                <iframe
+                    title="Ratio Student and Dosen - Ratio Mahasiswa Dosen"
+                    // width="600" height="373.5" 
+                    className='w-full h-screen'
+                    src="https://app.powerbi.com/view?r=eyJrIjoiMmNmMzNmYzktNGJjOS00M2VlLWJkMmMtYThkOTZiZTMyMjU3IiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+
+            </div>
+        </Layout>
+    )
+}
+
+export default MahasiswaBaru
