16e7c0a8d00044304d2fb08838631637b89dbd0c
Febe Putrianne <putrifebe@gmail.com>
2023-01-03 14:14:24 +0700

    gittest

--- sections/DataMahasiswa.jsx
+++ sections/DataMahasiswa.jsx
@@ -7,7 +7,7 @@ const DataMahasiswa = () => {
             title="All student dashboard 161222 - Halaman 1" 
             // width="600" height="373.5" 
             className='w-full h-[200vh]'
-            src="https://app.powerbi.com/view?r=eyJrIjoiY2I5ODVmNDItY2ZjOS00N2RkLWIxMWItYjExNGQwZjBiMWZkIiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+            src="https://app.powerbi.com/view?r=eyJrIjoiYmZjYjViNDQtMTZmYi00MmMzLWFlY2EtOWIwMTAwZTY3MDIwIiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D&pageName=ReportSection4433fe8dc98a0e8f41f3" frameborder="0" allowFullScreen="true"></iframe>
 
         </div>
     )
