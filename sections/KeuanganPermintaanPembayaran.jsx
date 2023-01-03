a50291ebab950dff54c1195fd492d205a2d393cc
Febe Putrianne <putrifebe@gmail.com>
2023-01-03 13:52:51 +0700

    gittest

--- sections/KeuanganPermintaanPembayaran.jsx
+++ sections/KeuanganPermintaanPembayaran.jsx
@@ -7,8 +7,8 @@ const KeuanganPermintaanPembayaran = () => {
                 title="Dashboard Keuangan Mahasiswa"
                 className='w-full h-screen scale-[1.1]'
                 // width="600" height="373.5"
-                src="https://app.powerbi.com/view?r=eyJrIjoiOWYxNmU5NDMtZmQ2MC00Y2ZlLWEwYTAtY2U3NWRjZWE4Yjc3IiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
-
+                src="https://app.powerbi.com/view?r=eyJrIjoiMGI2NWM3MDMtYzMzNy00ZWQwLWExOTktNWQyY2Q1Yjk0MDE4IiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+``
         </div>
     )
 }
