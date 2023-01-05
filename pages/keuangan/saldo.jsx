1e5ddb45d8e3393be681488193c53e9ff00b8a50
Febe Putrianne <putrifebe@gmail.com>
2023-01-05 14:49:14 +0700

    gittest

--- pages/keuangan/saldo.jsx
+++ pages/keuangan/saldo.jsx
@@ -8,10 +8,10 @@ const Saldo = () => {
         >
             <div className='mt-[10rem]'>
                 <iframe
-                    title="Saldo Dashboard 040123 - Halaman 1"
+                    title="Saldo Dashboard - Halaman 1"
                     className='w-full h-screen scale-[1.1]'
                     // width="600" height="373.5"
-                    src="https://app.powerbi.com/view?r=eyJrIjoiMWQwYTYxN2UtOWRjZC00YzI0LWFiNTMtOTI1MjVjN2VkNDdhIiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+                    src="https://app.powerbi.com/view?r=eyJrIjoiZDM4YmQyMmYtNjRjNS00ODkyLWIwNDUtMGU2ZWEwZWZkMzY2IiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
             </div>
         </Layout>
     )
