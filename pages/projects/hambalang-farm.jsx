1283edef90013e425ed0b0d6966afeeedff9ec05
Febe Putrianne <putrifebe@gmail.com>
2023-01-12 17:41:28 +0700

    gittest

--- pages/projects/hambalang-farm.jsx
+++ pages/projects/hambalang-farm.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import Layout from '../../components/Layout'
 
-const Pressensi = () => {
+const HambalangFarm = () => {
     return (
         <Layout
-            pageTitle='Pressensi'
+            pageTitle='Hambalang Farm'
         >
             <div className='pt-[10rem]'>
                 <iframe
-                    title="UMB Pressensi Dashboard 040123 - Page 1"
+                    title="Hambalang Farm 120123 - Halaman 1"
                     // width="600" height="373.5" 
                     className='w-full h-screen'
-                    src="https://app.powerbi.com/view?r=eyJrIjoiODY5MmE3ZjYtMjUzYS00OGNiLTlhMmYtOTM3N2RkMGY2OTc1IiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+                    src="https://app.powerbi.com/view?r=eyJrIjoiYzBmMzJhZjgtZTEwMS00NTU1LTg3NzYtNjI2YmIzMDhkZGJlIiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
 
             </div>
         </Layout>
     )
 }
 
-export default Pressensi
+export default HambalangFarm
