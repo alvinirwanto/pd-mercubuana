18d1b974c2b8eaac0f2f5f3c0e5433989892a596
Febe Putrianne <putrifebe@gmail.com>
2023-01-19 10:10:37 +0700

    gittest

--- pages/keuangan/laporan-keuangan.jsx
+++ pages/keuangan/laporan-keuangan.jsx
@@ -10,7 +10,7 @@ const LaporanKeuangan = () => {
                 <iframe
                     title="Laporan Keuangan as of December 31, 2022"
                     className='w-full h-screen scale-[1.1]'
-                    // width="600" height="373.5"
+                    // width="600" height="373.5"0
                     src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSEr0tmW50N_94dxpVlEWuvfPi2mBBWmvjqSldIpToxuTQG-0QlDSlcA7yejbz1cVZSXVJmGbwBGuUs/pubhtml?widget=true&amp;headers=false" frameborder="0" allowFullScreen="true"></iframe>
             </div>
         </Layout>
