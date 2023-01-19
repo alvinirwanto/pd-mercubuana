4bd356c3f16d87a3e08660f7ca37fa6cf38d9357
Febe Putrianne <putrifebe@gmail.com>
2023-01-19 09:36:17 +0700

    gittest

---
+++ pages/projects/hambalang-farm.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const HambalangFarm = () => {
+    return (
+        <Layout
+            pageTitle='Hambalang Farm'
+        >
+            <div className='pt-[10rem]'>
+                <iframe
+                    title="Hambalang Farm 120123 - Halaman 1"
+                    // width="600" height="373.5" 
+                    className='w-full h-screen'
+                    src="https://app.powerbi.com/view?r=eyJrIjoiYzBmMzJhZjgtZTEwMS00NTU1LTg3NzYtNjI2YmIzMDhkZGJlIiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+
+            </div>
+        </Layout>
+    )
+}
+
+export default HambalangFarm
