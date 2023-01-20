0565ea130a1e8e8a461d97f4f7783a118a5ca448
Febe Putrianne <putrifebe@gmail.com>
2023-01-20 11:13:22 +0700

    gittest

--- pages/proyek/hambalang-farm.jsx
+++ pages/proyek/hambalang-farm.jsx
@@ -11,7 +11,7 @@ const HambalangFarm = () => {
                     title="Hambalang Farm 120123 - Halaman 1"
                     // width="600" height="373.5" 
                     className='w-full h-screen'
-                    src="https://app.powerbi.com/view?r=eyJrIjoiNTdlMTBiZGUtOTQwMS00YWY5LWE2ZGUtNWJiZjE0MTZmMDlhIiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+                    src="https://app.powerbi.com/view?r=eyJrIjoiOGJkM2FiODctNzJiYy00ZmI1LThlZTgtMDc4NjMyOTMwZjY3IiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
 
             </div>
         </Layout>
