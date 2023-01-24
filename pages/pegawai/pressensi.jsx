9977576d822b970f691e8b1d6dcdb1def8553f91
Febe Putrianne <putrifebe@gmail.com>
2023-01-24 11:30:16 +0700

    gittest

--- pages/pegawai/pressensi.jsx
+++ pages/pegawai/pressensi.jsx
@@ -8,10 +8,10 @@ const Pressensi = () => {
         >
             <div className='pt-[10rem]'>
                 <iframe
-                    title="UMB Pressensi Dashboard 040123 - Page 1"
+                    title="UMB Pressensi Dashboard 240123 - Page 1"
                     // width="600" height="373.5" 
                     className='w-full h-screen'
-                    src="https://app.powerbi.com/view?r=eyJrIjoiODY5MmE3ZjYtMjUzYS00OGNiLTlhMmYtOTM3N2RkMGY2OTc1IiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+                    src="https://app.powerbi.com/view?r=eyJrIjoiYjYwZWEzNzktNWExNy00ODIxLTgzYzAtOGRmOTkzNTk0ZGJhIiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
 
             </div>
         </Layout>
