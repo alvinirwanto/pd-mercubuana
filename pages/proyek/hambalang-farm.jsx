851b99360014aead8b31a1fa7d2ad3d47df8d9df
Febe Putrianne <putrifebe@gmail.com>
2023-01-24 16:03:21 +0700

    febe gaje 2

--- pages/proyek/hambalang-farm.jsx
+++ pages/proyek/hambalang-farm.jsx
@@ -1,21 +1,21 @@
-// import React from 'react'
-// import Layout from '../../components/Layout'
+import React from 'react'
+import Layout from '../../components/Layout'
 
-// const HambalangFarm = () => {
-//    return (
-//    <Layout
-//         pageTitle='Hambalang Farm'
-//         >
-//             <div className='pt-[10rem]'>
-//                 <iframe
-//                     title="Hambalang Farm 120123 - Halaman 1"
-//                      width="600" height="373.5" 
-//                     className='w-full h-screen'
-//                    src="https://app.powerbi.com/view?r=eyJrIjoiOGJkM2FiODctNzJiYy00ZmI1LThlZTgtMDc4NjMyOTMwZjY3IiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+const HambalangFarm = () => {
+   return (
+   <Layout
+        pageTitle='Hambalang Farm'
+        >
+            <div className='pt-[10rem]'>
+                <iframe
+                    title="Hambalang Farm 120123 - Halaman 1"
+                     width="600" height="373.5" 
+                    className='w-full h-screen'
+                   src="https://app.powerbi.com/view?r=eyJrIjoiOGJkM2FiODctNzJiYy00ZmI1LThlZTgtMDc4NjMyOTMwZjY3IiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
 
-//            </div>
-//         </Layout>
-//    )
-//     }
+           </div>
+        </Layout>
+   )
+    }
 
-// export default HambalangFarm
+export default HambalangFarm
