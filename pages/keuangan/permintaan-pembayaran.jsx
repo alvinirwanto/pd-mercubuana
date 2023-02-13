6b9a950b7cb5e1155dbbc6530c4012c754ea5d31
Febe Putrianne <putrifebe@gmail.com>
2023-02-13 11:23:55 +0700

    gittest

--- pages/keuangan/permintaan-pembayaran.jsx
+++ pages/keuangan/permintaan-pembayaran.jsx
@@ -8,7 +8,7 @@ const PermintaanPembayaran = () => {
         >
             <div className='mt-[10rem]'>
                 <iframe
-                    title="Dashboard Keuangan Mahasiswa"
+                    title="Pengajuan PP dan PUM"
                     className='w-full h-screen scale-[1.1]'
                     // width="600" height="373.5"
                     src="https://app.powerbi.com/view?r=eyJrIjoiZTQ5MzlkZWYtMTRkMi00MjQ4LWE2OTYtNWYzYzNiNWI0NDgwIiwidCI6ImZlOTZiY2VlLTc0YjEtNDQxNS1hNWE4LTZiYmJlNGNmMjUxMiIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
